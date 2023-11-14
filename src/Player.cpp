8f6236b11959e5dedf22a6365933faf1bd7ee86a
drafolin <derg@drafolin.ch>
2023-11-14 14:56:02 +0100

    added player class

---
+++ src/Player.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Dråfølin on 14.11.2023.
+//
+
+#include "player.h"
+#include "resources.h"
+#include "utilities.h"
+
+Player::Player(): Sprite(GameFramework::imagesPath() + "/player.png")
+{
+
+}
---
+++ src/Player.h
@@ -0,0 +1,17 @@
+//
+// Created by Dråfølin on 14.11.2023.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "sprite.h"
+
+class Player: public Sprite {
+    public:
+    Player();
+};
+
+
+
+#endif //PLAYER_H
