8e1fe6d2dae93d17191e786487fc4be27bb36b46
drafolin <derg@drafolin.ch>
2024-01-20 23:08:05 +0100

    added fragile platform texture

--- src/fragileplatform.cpp
+++ src/fragileplatform.cpp
@@ -5,14 +5,47 @@
 #include "fragileplatform.h"
 
 #include <gamescene.h>
+#include <QPainter>
+#include <QRandomGenerator>
 
 #include "body.h"
 
 //! Constructeur de la classe FragilePlatform
 //! \param rect rectangle de la plateforme
 FragilePlatform::FragilePlatform(const QRect rect) : Platform(rect) {
-    m_pImage = new QImage(rect.width(), rect.height(), QImage::Format_ARGB32);
-    m_pImage->fill(Qt::cyan);
+    m_pImage = new QImage(rect.width(), rect.height(), QImage::Format_RGBA64);
+    m_pImage->fill(QColor::fromRgb(
+            148,
+            148,
+            148
+    ));
+    QPainter painter(m_pImage);
+    auto detailColor = QColor::fromRgb(
+            80,
+            80,
+            80
+    );
+    painter.setPen(detailColor);
+    painter.drawRect(0, 0, rect.width() - 1, rect.height() - 1);
+
+    for (auto y = 0; y < rect.height(); y++) {
+        auto line = (QRgba64 *) m_pImage->scanLine(y);
+        for (auto x = 0; x < rect.width(); x++) {
+            // 20 % de chance de dessiner la rouille
+            qDebug() << QRandomGenerator::global()->bounded(0, 100);
+            if (QRandomGenerator::global()->bounded(0, 100) > 20)
+                continue;
+
+            QRgba64 &rgb = line[x];
+            rgb = QRgba64::fromRgba(
+                    124,
+                    82,
+                    59,
+                    255
+            );
+        }
+    }
+
     setPixmap(QPixmap::fromImage(*m_pImage));
 }
 
