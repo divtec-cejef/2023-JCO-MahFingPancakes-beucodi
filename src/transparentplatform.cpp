39264ab031c2bc5432883416d60a46d5ea9ae313
drafolin <derg@drafolin.ch>
2024-01-20 23:09:21 +0100

    added transparent platform texture

--- src/transparentplatform.cpp
+++ src/transparentplatform.cpp
@@ -2,13 +2,48 @@
 // Created by Dråfølin on 05.12.2023.
 //
 
+#include <QPainter>
 #include "transparentplatform.h"
 #include "body.h"
 
 //! Constructeur de la classe TransparentPlatform
 TransparentPlatform::TransparentPlatform(const QRect rect) : Platform(rect) {
-    m_pImage = new QImage(rect.width(), rect.height(), QImage::Format_ARGB32);
-    m_pImage->fill(Qt::red);
+    m_pImage = new QImage(rect.width(), rect.height(), QImage::Format_RGBA64);
+    m_pImage->fill(QColor::fromRgb(
+            148,
+            148,
+            148
+    ));
+    QPainter painter(m_pImage);
+    auto detailColor = QColor::fromRgb(
+            80,
+            80,
+            80
+    );
+    painter.setPen(detailColor);
+    painter.drawRect(0, 0, rect.width() - 1, rect.height() - 1);
+
+    const int boltSize = 4;
+    const int boltOffset = 3;
+
+    // Bolts
+    painter.fillRect(
+            QRect(
+                    boltOffset,
+                    boltOffset,
+                    boltSize,
+                    boltSize
+            ),
+            detailColor
+    );
+    painter.fillRect(
+            QRect(rect.width() - boltOffset - boltSize,
+                  boltOffset,
+                  boltSize,
+                  boltSize
+            ),
+            detailColor
+    );
     setPixmap(QPixmap::fromImage(*m_pImage));
 }
 
