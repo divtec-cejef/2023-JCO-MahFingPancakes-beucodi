00ddd9225d4972879c580b3722882fcc8c4c8f04
drafolin <derg@drafolin.ch>
2024-01-22 18:50:52 +0100

    replaced echap with esc icon

--- src/gamecore.cpp
+++ src/gamecore.cpp
@@ -13,17 +13,19 @@
 #include "levelbuilder.h"
 #include "level.h"
 #include "utilities.h"
+#include "resources.h"
 
 #include "player.h"
 #include "transparentplatform.h"
 
 constexpr int SCENE_WIDTH = 1280;
+constexpr int SCENE_HEIGHT = SCENE_WIDTH * 9 / 16;
 
 //! Initialise le contrôleur de jeu.
 //! \param pGameCanvas  GameCanvas pour lequel cet objet travaille.
 //! \param pParent      Pointeur sur le parent (afin d'obtenir une destruction automatique de cet objet).
 GameCore::GameCore(GameCanvas *pGameCanvas, QObject *pParent) : QObject(pParent) {
-    // Mémorise l'accès au canvas (qui gère le tick et l'affichage d'une scène)
+    // Mémorise l’accès au canvas (qui gère le tick et l’affichage d’une scène)
     m_pGameCanvas = pGameCanvas;
 
     m_pPlayer = new Player();
@@ -37,11 +39,12 @@ GameCore::GameCore(GameCanvas *pGameCanvas, QObject *pParent) : QObject(pParent)
     m_pPlayer->initialize();
     m_pLevel->initialize();
 
-    // Démarre le tick pour que les animations qui en dépendent fonctionnent correctement.
-    // Attention : il est important que l'enclenchement du tick soit fait vers la fin de cette fonction,
-    // sinon le temps passé jusqu'au premier tick (ElapsedTime) peut être élevé et provoquer de gros
-    // déplacements, surtout si le déboggueur est démarré.
+    // Commence le tick pour que les animations qui en dépendent fonctionnent correctement.
+    // Attention : il est important que l’enclenchement du tick soit fait vers la fin de cette fonction,
+    // sinon le temps passé jusqu’au premier tick (ElapsedTime) peut être élevé et provoquer de gros
+    // déplacements, surtout si le débogueur est démarré.
     m_pGameCanvas->startTick();
+    onPlayerDied();
 }
 
 //! Change le niveau actuel.
@@ -85,8 +88,7 @@ void GameCore::keyPressed(const int key) {
             m_pLevel->deleteLater();
         }
 
-        if (m_pPlayer != nullptr)
-            delete m_pPlayer;
+        delete m_pPlayer;
 
         m_pPlayer = new Player();
         connect(this, &GameCore::notifyKeyPressed, m_pPlayer, &Player::keyPressed);
@@ -151,7 +153,7 @@ void GameCore::onPlayerDied() {
     m_pPlayer->pack();
     m_pLevel->deleteLater();
     m_pLevel = nullptr;
-    auto newScene = m_pGameCanvas->createScene();
+    auto newScene = m_pGameCanvas->createScene(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
     m_pGameCanvas->setCurrentScene(newScene);
     m_pPlayer->deleteLater();
     m_pPlayer = nullptr;
@@ -161,20 +163,64 @@ void GameCore::onPlayerDied() {
     QPainter messagePainter(&deathMessagePixmap);
     messagePainter.setFont(QFont("Arial", 32));
     messagePainter.setPen(QPen(QColorConstants::Red));
-    messagePainter.drawText(QRectF(0, 0, 600, 200), Qt::AlignCenter, "Vous êtes mort !");
+    messagePainter.drawText(QRectF(0, 0, 600, 200), Qt::AlignCenter, "Vous êtes mort !");
     auto spriteMsg = new Sprite(deathMessagePixmap);
     newScene->addSpriteToScene(spriteMsg);
-    spriteMsg->setPos(SCENE_WIDTH / 2.0 - spriteMsg->width(), 200 - spriteMsg->height());
+    spriteMsg->setPos(SCENE_WIDTH / 2.0 - spriteMsg->width() / 2.0, 200 - spriteMsg->height() / 2.0);
 
-    auto tooltipPixmap = QPixmap(600, 200);
+    const int toolTipWidth = 600;
+    const int toolTipHeight = 200;
+    const int keySpriteWidth = 40;
+
+    auto tooltipPixmap = QPixmap(toolTipWidth, toolTipHeight);
     tooltipPixmap.fill(QColorConstants::Transparent);
     QPainter tooltipPainter(&tooltipPixmap);
     tooltipPainter.setFont(QFont("Arial", 16));
     tooltipPainter.setPen(QPen(QColorConstants::White));
-    tooltipPainter.drawText(QRectF(0, 0, 600, 200), Qt::AlignCenter, "Appuyez sur Echap pour redémarrer");
+    QRect requiredBounding;
+    tooltipPainter.drawText(
+            QRect(
+                    0,
+                    0,
+                    toolTipWidth / 2.0,
+                    toolTipHeight
+            ),
+            Qt::AlignLeft | Qt::AlignVCenter,
+            "Appuyez sur",
+            &requiredBounding
+    );
+
+    auto keySprite = QImage();
+    keySprite.load(
+            QString("%1/keys/Esc_Key_Light.png")
+                    .arg(GameFramework::imagesPath())
+    );
+
+    tooltipPainter.drawImage(
+            QPoint(
+                    requiredBounding.width(),
+                    toolTipHeight / 2.0 - keySpriteWidth / 2.0
+            ),
+            keySprite.scaledToWidth(keySpriteWidth, Qt::SmoothTransformation)
+    );
+
+
+    tooltipPainter.drawText(
+            QRectF(
+                    requiredBounding.width() + keySpriteWidth,
+                    0,
+                    toolTipWidth / 2.0 - keySpriteWidth,
+                    toolTipHeight
+            ),
+            Qt::AlignLeft | Qt::AlignVCenter,
+            "pour recharger le jeu."
+    );
     auto spriteTooltip = new Sprite(tooltipPixmap);
     newScene->addSpriteToScene(spriteTooltip);
-    spriteTooltip->setPos(SCENE_WIDTH / 2.0 - spriteTooltip->width(), 400 - spriteTooltip->height());
+    spriteTooltip->setPos(
+            SCENE_WIDTH / 2.0 - spriteTooltip->shape().boundingRect().width() / 2.0,
+            400 - toolTipHeight / 2.0
+    );
 
     m_pGameCanvas->startTick();
 }
